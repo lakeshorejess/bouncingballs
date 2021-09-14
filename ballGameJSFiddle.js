14c4b75dedbd8378a2f5dc86ce2b987265b0c051
Jessica Lindsey <89706140+lakeshorejess@users.noreply.github.com>
2021-09-13 21:41:52 -0600

    Add files via upload

---
+++ ballGameJSFiddle.js
@@ -0,0 +1,135 @@
+var ball = document.getElementById("ball");
+var velocityX = 10;
+var velocityY = 10;
+var positionX = 0;
+var positionY = 0;
+var sideToSide = false;
+var upAndDown = false;
+
+function moveBall() {
+  ball.style.left = positionX + 'px';
+  ball.style.top = positionY + 'px';
+  if(sideToSide === false){
+    positionX += velocityX;
+  } else {
+    positionX -= velocityX;
+  }
+  if(upAndDown === false){
+    positionY += velocityY;
+  } else {
+    positionY -= velocityY;
+  }
+ 
+  var Xmin = 0;
+  var Xmax = 500;
+  var Ymin = 0;
+  var Ymax = 250;
+  if(positionX > Xmax){
+    sideToSide = true;
+    ball.style.background = '#F3F781';
+  }
+  if(positionX === Xmin){
+    sideToSide = false;
+    ball.style.background = '#4B088A';
+  }
+  if(positionY > Ymax){
+    upAndDown = true;
+    ball.style.background = '#F3F781'
+  }
+  if(positionY === Ymin){
+    upAndDown = false;
+    ball.style.background = '#4B088A'
+  }
+}
+
+setInterval(moveBall, 100);
+
+var ball2 = document.getElementById("ball2");
+var velocityX2 = 5;
+var velocityY2 = 5;
+var positionX2 = 0;
+var positionY2 = 0;
+var sideToSide2 = false;
+var upAndDown2 = false;
+
+function moveBall2() {
+  ball2.style.left = positionX2 + 'px';
+  ball2.style.top = positionY2 + 'px';
+  if(sideToSide === false){
+    positionX2 += velocityX2;
+  } else {
+    positionX2 -= velocityX2;
+  }
+  if(upAndDown === false){
+    positionY2 += velocityY2;
+  } else {
+    positionY2 -= velocityY2;
+  }
+  
+  var Xmin2 = 0;
+  var Xmax2 = 400;
+  var Ymin2 = 0;
+  var Ymax2 = 150;
+  if(positionX2 > Xmax2){
+    sideToSide2 = true;
+    ball2.style.background = '#04B486'
+  }
+  if(positionX2 === Xmin2){
+    sideToSide2 = false;
+    ball2.style.background = '#8A084B'
+  }
+  if(positionY2 > Ymax2){
+    upAndDown2 = true;
+    ball2.style.background = '#FF0000'
+  }
+  if(positionY2 === Ymin2){
+    upAndDown2 = false;
+    ball2.style.background = '#1C1C1C'
+  }
+}
+setInterval(moveBall2, 100);
+
+var ball3 = document.getElementById("ball3");
+var velocityX3 = 15;
+var velocityY3 = 15;
+var positionX3 = 0;
+var positionY3 = 0;
+var sideToSide3 = false;
+var upAndDown3 = false;
+
+function moveBall3() {
+  ball3.style.left = positionX3 + 'px';
+  ball3.style.top = positionY3 + 'px';
+  if(sideToSide3 === false){
+    positionX3 += velocityX3;
+  } else {
+    positionX3 -= velocityX3;
+  }
+  if(upAndDown3 === false){
+    positionY3 += velocityY3;
+  } else {
+    positionY3 -= velocityY3;
+  }
+
+  var Xmin3 = 0;
+  var Xmax3 = 500;
+  var Ymin3 = 0;
+  var Ymax3 = 200;
+  if(positionX3 > Xmax3){
+    sideToSide3 = true;
+    ball3.style.background = '#170B3B'
+  }
+  if(positionX3 === Xmin3){
+    sideToSide3 = false;
+     ball3.style.background = '#0B615E'
+  }
+  if(positionY3 > Ymax3){
+    upAndDown3 = true;
+     ball3.style.background = '#A9F5F2'
+  }
+  if(positionY3 === Ymin3){
+    upAndDown3 = false;
+     ball3.style.background = '#F781BE'
+  }
+}
+setInterval(moveBall3, 100);
